b13bf9696be67a68b56d9428a1e1bdd2caf57d93
ravinder <ravibeniwal931@gmail.com>
2025-07-01 14:54:47 +0530

    01 : setup

---
+++ dripui/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='font-bold'>
+      <Button variant="new"> click me</Button>
+    </div>
+  )
+}
+
+export default page
