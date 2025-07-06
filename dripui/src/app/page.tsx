d5923331132d894da56ef8fb9894520a4c43500d
ravinder <ravibeniwal931@gmail.com>
2025-07-06 21:19:52 +0530

    trpc setup

--- dripui/src/app/page.tsx
+++ dripui/src/app/page.tsx
@@ -1,12 +1,26 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
 
-const page = () => {
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import React from 'react'; 
+import Client from './client';
+import { Suspense } from 'react';
+
+
+const Page = async() => {
+  const queryClient = getQueryClient(); 
+  void  queryClient.prefetchQuery(trpc.hello.queryOptions({ text: 'trpc' })); 
+   
+ 
   return (
-    <div className='font-bold'>
-      <Button variant="new"> click me</Button>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className='font-bold'>
+      <Client/>
     </div>
+      </Suspense>
+       
+    </HydrationBoundary>
   )
 }
 
-export default page
+export default Page
